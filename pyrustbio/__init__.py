73c918d499a6b2838f600dee4f6fe704030864c1
jeff-k <jeff@x-0.ca>
2018-02-22 15:42:13 -0800

    negate gap penalties to match MiCall API

--- pyrustbio/__init__.py
+++ pyrustbio/__init__.py
@@ -8,6 +8,6 @@ def levenshtein(a, b):
     return _pyrustbio.levenshtein(a, b)
 
 def align_it(ref, query, g_o, g_e, terminal_p):
-    score, pretty = _pyrustbio.affine(query, ref, g_o, g_e)
+    score, pretty = _pyrustbio.affine(query, ref, -1 * g_o, -1 * g_e)
     ref_a, _, query_a = pretty.split('\n')[0:3]
     return ref_a.replace(' ', '-'), query_a.replace(' ', '-'), score

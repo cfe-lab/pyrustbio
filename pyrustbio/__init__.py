02386fee428812bd24bf6e0a7cd9d3656ab2aa51
jeff-k <jeff@x-0.ca>
2018-03-13 14:38:58 -0700

    enable construction of match scoring closure

--- pyrustbio/__init__.py
+++ pyrustbio/__init__.py
@@ -1,13 +1,13 @@
 from . import _pyrustbio
 
-def pairwise(query, ref, g_o, g_e):
-    score, pretty = _pyrustbio.affine(query, ref, g_o, g_e)
+def pairwise(query, ref, g_o, g_e, match=5, mm=-4):
+    score, pretty = _pyrustbio.affine(query, ref, g_o, g_e, match, mm)
     return score, pretty.split('\n')[0:3]
 
 def levenshtein(a, b):
     return _pyrustbio.levenshtein(a, b)
 
 def align_it(ref, query, g_o, g_e, terminal_p):
-    score, pretty = _pyrustbio.affine(query, ref, -1 * g_o, -1 * g_e)
+    score, pretty = _pyrustbio.affine(query, ref, -1 * g_o, -1 * g_e, 5, -4)
     ref_a, _, query_a = pretty.split('\n')[0:3]
     return ref_a.replace(' ', '-'), query_a.replace(' ', '-'), score
--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ use pyo3::prelude::*;
 
 use bio::alignment::distance::levenshtein;
 use bio::alignment::pairwise::Aligner;
-use bio::scores::blosum62::blosum62;
+//use bio::scores::blosum62::blosum62;
 
 #[py::modinit(_pyrustbio)]
 fn init(py: Python, m: &PyModule) -> PyResult<()> {
@@ -19,8 +19,15 @@ fn init(py: Python, m: &PyModule) -> PyResult<()> {
 
     #[pyfn(m, "affine")]
     fn py_affine(_py: Python, q: String, r: String,
-                  gop: i32, gep: i32) -> PyResult<&PyList> {
-        let mut a = Aligner::with_capacity(q.len(), r.len(), gop, gep, &blosum62);
+                  gop: i32, gep: i32, m: i32, mm: i32) -> PyResult<&PyList> {
+        let m = |a, b| -> i32 {
+            if a == b {
+                return m
+            } else {
+                return mm
+            }
+        };
+        let mut a = Aligner::with_capacity(q.len(), r.len(), gop, gep, &m);
         let (q_b, r_b) = (q.into_bytes(), r.into_bytes());
         let alnmnt = a.semiglobal(&q_b, &r_b);
 
